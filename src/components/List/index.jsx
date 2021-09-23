7d64567ac069381d6d81dc2e6684422c79b849f4
HP <mega.pe4enka@gmail.com>
2021-09-23 21:07:06 +0300

    updated

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 import classNames from 'classnames'
 import Badge from '../Badge'
 
+import removeSvg from '../../assets/img/remove.svg'
+
 import './List.scss'
 
-const List = ({ items, isRemovable, onClick }) => {
+const List = ({ items, isRemovable, onClick, onRemove }) => {
+
+    const removeList = (item) => {
+        if (window.confirm('Вы действительно хотите удалить список?')) {
+            onRemove(item)
+        }
+    }
+
     return (
         <ul onClick={onClick} className="list">
             {
@@ -12,6 +21,13 @@ const List = ({ items, isRemovable, onClick }) => {
                     <li key={index} className={classNames(item.className, {active: item.active})}>
                         <i>{item.icon ? item.icon : <Badge color={item.color} />}</i>
                         <span>{item.name}</span>
+                        {isRemovable && (
+                        <img
+                            className='list__remove-icon'
+                            src={removeSvg}
+                            alt="Remove icon"
+                            onClick={() => removeList(item)}
+                        />)}
                     </li>
                 ))
             }
