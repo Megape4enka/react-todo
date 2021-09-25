4a88f13ed68386d012cb997c0c4f2eaa60fd5444
HP <mega.pe4enka@gmail.com>
2021-09-25 18:07:25 +0300

    Remove tasks

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React from 'react'
 import classNames from 'classnames'
+import axios from 'axios'
+
 import Badge from '../Badge'
 
 import removeSvg from '../../assets/img/remove.svg'
@@ -8,9 +10,11 @@ import './List.scss'
 
 const List = ({ items, isRemovable, onClick, onRemove }) => {
 
-    const removeList = (item) => {
+    const removeList = item => {
         if (window.confirm('Вы действительно хотите удалить список?')) {
-            onRemove(item)
+            axios.delete('http://localhost:3001/lists/' + item.id).then(() => {
+                onRemove(item.id)
+            })
         }
     }
 
@@ -19,15 +23,15 @@ const List = ({ items, isRemovable, onClick, onRemove }) => {
             {
                 items.map((item, index) => (
                     <li key={index} className={classNames(item.className, {active: item.active})}>
-                        <i>{item.icon ? item.icon : <Badge color={item.color} />}</i>
+                        <i>{item.icon ? item.icon : <Badge color={item.color.name} />}</i>
                         <span>{item.name}</span>
                         {isRemovable && (
-                        <img
-                            className='list__remove-icon'
-                            src={removeSvg}
-                            alt="Remove icon"
-                            onClick={() => removeList(item)}
-                        />)}
+                            <img
+                                className='list__remove-icon'
+                                src={removeSvg}
+                                alt="Remove icon"
+                                onClick={() => removeList(item)}
+                            />)}
                     </li>
                 ))
             }
