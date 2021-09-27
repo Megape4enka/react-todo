83b62dd2e84371fbb947cd29aa874d831bbe6357
HP <mega.pe4enka@gmail.com>
2021-09-27 20:48:45 +0300

    Updated

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-import './index.scss';
+import App from './App'
+
+import './index.scss'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
+)
 
